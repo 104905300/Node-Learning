3750b637537addb28fe18586185367759fb40318
Danny Liu <dannyliu810@gmail.com>
2022-08-03 21:17:43 -0400

    new file

--- node.js
+++ node.js
@@ -4,6 +4,7 @@ const {writeFile} = require("fs")
 console.log(`Hello ${os.uptime()} World!`)
 
 writeFile("./testing.txt", "I LOVE YOU!", (err,result) => {})
+writeFile("./testing2.txt", "I LOVE YOU TOO!", (err,result) => {})
 
-//sdfw fwf wff
+
 
