6b0419334864bf669e1430635a61e677bb611177
John Doe <dannyliu810@gmail.com>
2022-08-03 21:27:25 -0400

    "comment"

--- node.js
+++ node.js
@@ -6,5 +6,5 @@ console.log(`Hello ${os.uptime()} World!`)
 writeFile("./testing.txt", "I LOVE YOU!", (err,result) => {})
 writeFile("./testing2.txt", "I LOVE YOU TOO!", (err,result) => {})
 
-
+//comment
 
